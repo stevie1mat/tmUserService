d29b580ef15a323dd2fadf3a5a1e0b42439eb150
Steven Mathew <mathewsteven92@gmail.com>
2025-08-15 12:42:33 -0400

    chore(cors): allow all headers and methods for cross-origin requests

--- main.go
+++ main.go
@@ -17,8 +17,8 @@ import (
 func CORSMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "*")
 
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusNoContent)
